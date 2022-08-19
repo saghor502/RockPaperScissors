a2451778a4a68992a909b283dfd03d1e26368240
Saghir <classicaghir@hotmail.com>
2022-08-18 18:11:22 -0600

    first commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Ángel Saghir Rodríguez Fernández on 18/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

34be3986875c78251e5beab3ae86d9e1d8f4bd19
Saghir <classicaghir@hotmail.com>
2022-08-23 14:19:22 -0600

    third commit

---
+++ RockPaperScissors/Model/ValueObject/Maquina.swift
@@ -0,0 +1,16 @@
+//
+//  Maquina.swift
+//  RockPaperScissors
+//
+//  Created by Ángel Saghir Rodríguez Fernández on 23/08/22.
+//
+
+import Foundation
+
+public class maquina{
+    func randomElement()->RPS {
+        let rps = [RPS.Rock, RPS.Paper, RPS.Scissors]
+        let randomRps = rps.randomElement()!
+        return(randomRps)
+    }
+}
---
+++ RockPaperScissors/Model/ValueObject/Resultado.swift
@@ -0,0 +1,51 @@
+//
+//  Resultado.swift
+//  RockPaperScissors
+//
+//  Created by Ángel Saghir Rodríguez Fernández on 23/08/22.
+//
+
+enum RPS{
+    case Rock
+    case Paper
+    case Scissors
+}
+
+import Foundation
+
+public class resultado{
+    let desicionJ: RPS
+    
+    init(desicionJ : RPS){
+        self.desicionJ = desicionJ
+    }
+    
+    func darResultado(desicionM : RPS, desicionJ : RPS)->String{
+        let desicionM = maquina().randomElement()
+        if(desicionM == RPS.Rock){
+            if(self.desicionJ == RPS.Rock){
+                return("D")
+            }else if(self.desicionJ == RPS.Paper){
+                return("J")
+            }else{
+                return("M")
+            }
+        }else if(desicionM == RPS.Paper){
+            if(self.desicionJ == RPS.Rock){
+                return("M")
+            }else if(self.desicionJ == RPS.Paper){
+                return("D")
+            }else{
+                return("J")
+            }
+        }else{
+            if(self.desicionJ == RPS.Rock){
+                return("J")
+            }else if(self.desicionJ == RPS.Paper){
+                return("M")
+            }else{
+                return("D")
+            }
+        }
+    }
+}
